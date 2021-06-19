9fb57d5efb0e65f90c302418f52eb8427008aa69
jon <lgock2023@gmail.com>
2021-06-19 12:23:26 +0900

    feat: firebase setting

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("io.springfox:springfox-swagger2:2.9.2")
     implementation("io.springfox:springfox-swagger-ui:2.9.2")
+    implementation("com.google.firebase:firebase-admin:7.1.0")
 }
