270a50e67681f36d561fff1633e365927acfbb4f
jon <lgock2023@gmail.com>
2021-07-18 18:13:46 +0900

    feature: logging in exception handler

--- api/src/main/kotlin/waffle/guam/config/ErrorHandler.kt
+++ api/src/main/kotlin/waffle/guam/config/ErrorHandler.kt
@@ -1,5 +1,7 @@
 package waffle.guam.config
 
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -12,19 +14,34 @@ import waffle.guam.exception.NotAllowedException
 
 @ControllerAdvice
 class ErrorHandler {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @ExceptionHandler(value = [DataNotFoundException::class])
-    fun notfound(e: RuntimeException) =
-        ResponseEntity(ErrorResponse(e.message ?: ""), HttpStatus.NOT_FOUND)
+    fun notfound(e: RuntimeException): ResponseEntity<ErrorResponse> {
+        logger.error(e.message, e)
+        return ResponseEntity(ErrorResponse(e.message ?: "", MDC.get("request-id")), HttpStatus.NOT_FOUND)
+    }
 
     @ExceptionHandler(value = [InvalidFirebaseTokenException::class, InvalidRequestException::class])
-    fun baeRequest(e: RuntimeException) =
-        ResponseEntity(ErrorResponse(e.message ?: ""), HttpStatus.BAD_REQUEST)
+    fun baeRequest(e: RuntimeException): ResponseEntity<ErrorResponse> {
+        logger.error(e.message, e)
+        return ResponseEntity(ErrorResponse(e.message ?: "", MDC.get("request-id")), HttpStatus.BAD_REQUEST)
+    }
 
     @ExceptionHandler(value = [JoinException::class, NotAllowedException::class])
-    fun notAllowed(e: RuntimeException) =
-        ResponseEntity(ErrorResponse(e.message ?: ""), HttpStatus.FORBIDDEN)
+    fun notAllowed(e: RuntimeException): ResponseEntity<ErrorResponse> {
+        logger.error(e.message,e )
+        return ResponseEntity(ErrorResponse(e.message ?: "", MDC.get("request-id")), HttpStatus.FORBIDDEN)
+    }
+
+    @ExceptionHandler(value = [Exception::class])
+    fun unknown(e: Exception): ResponseEntity<ErrorResponse> {
+        logger.error(e.message, e)
+        return ResponseEntity(ErrorResponse(e.message ?: "알 수 없는 에러입니다.", MDC.get("request-id")), HttpStatus.FORBIDDEN)
+    }
 }
 
 data class ErrorResponse(
-    val message: String
+    val message: String,
+    val requestId: String?
 )
