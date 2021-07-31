d3bbadd76f3b4795fdfa92f2cbdade53f6836935
jon <lgock2023@gmail.com>
2021-07-31 15:30:04 +0900

    [GUAM-90] feat: new API for UserController

--- api/src/main/kotlin/waffle/guam/controller/UserController.kt
+++ api/src/main/kotlin/waffle/guam/controller/UserController.kt
@@ -3,6 +3,7 @@ package waffle.guam.controller
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,13 +25,19 @@ class UserController(
 ) {
     private val mapper = jacksonObjectMapper()
 
-    @GetMapping("")
+    @GetMapping("/me")
     fun getUser(
         userContext: UserContext
     ): SuccessResponse<User> =
         SuccessResponse(
             userService.get(userContext.id)
         )
+
+    @GetMapping("/{id}")
+    fun getUser(
+        @PathVariable id: Long
+    ): SuccessResponse<User> =
+        SuccessResponse(userService.get(id))
 
     @PostMapping("")
     fun updateUser(
