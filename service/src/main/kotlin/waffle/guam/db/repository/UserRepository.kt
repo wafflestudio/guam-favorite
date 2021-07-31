9a85dae7abd02efb3d07b1fb5a762f9ab28a7846
jon <lgock2023@gmail.com>
2021-07-31 15:27:56 +0900

    [GUAM-90] feat: select user with image, tasks at once

--- service/src/main/kotlin/waffle/guam/db/repository/UserRepository.kt
+++ service/src/main/kotlin/waffle/guam/db/repository/UserRepository.kt
@@ -1,8 +1,13 @@
 package waffle.guam.db.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import waffle.guam.db.entity.UserEntity
+import java.util.Optional
 
 interface UserRepository : JpaRepository<UserEntity, Long> {
+    @Query("select u from UserEntity u left join fetch u.image join fetch u.tasks t join fetch t.project where u.id = :userId")
+    override fun findById(userId: Long): Optional<UserEntity>
+
     fun findByFirebaseUid(firebaseUid: String): UserEntity?
 }
