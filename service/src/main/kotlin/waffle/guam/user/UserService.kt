f897477a4186e050c0646f499ae1eb6c06fc712b
jon <lgock2023@gmail.com>
2021-08-04 21:27:47 +0900

    [NT]: add UserState to UserProject

--- service/src/main/kotlin/waffle/guam/user/UserService.kt
+++ service/src/main/kotlin/waffle/guam/user/UserService.kt
@@ -1,10 +1,10 @@
 package waffle.guam.user
 
-import waffle.guam.model.User
 import waffle.guam.user.command.ExtraFieldParams
 import waffle.guam.user.command.UpdateUser
 import waffle.guam.user.event.DeviceUpdated
 import waffle.guam.user.event.UserUpdated
+import waffle.guam.user.model.User
 
 interface UserService {
     fun getUser(firebaseUid: String, extraFieldOptions: ExtraFieldParams): User
