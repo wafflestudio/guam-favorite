58be6f123b1ba72ec23af358687a852645631251
Jeong Satchel <bugod96@gmail.com>
2021-08-06 18:44:28 +0900

    [GUAM-86] feat: aws siksha - maria db connection config

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
 //    runtimeOnly("com.h2database:h2")
-    runtimeOnly("mysql:mysql-connector-java")
+    implementation("org.mariadb.jdbc:mariadb-java-client:2.7.2")
 }
 
 dependencies {
