fd2317e8ea13b9ff70e4427ec1dd62161db31745
jon <lgock2023@gmail.com>
2021-08-10 23:31:17 +0900

    [GUAM-118] feat: ImageServiceImpl

---
+++ service/src/main/kotlin/image/ImageServiceImpl.kt
@@ -0,0 +1,61 @@
+package waffle.guam.image
+
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.PutObjectRequest
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import waffle.guam.image.command.CreateImages
+import waffle.guam.image.command.DeleteImages
+import waffle.guam.image.event.ImagesCreated
+import waffle.guam.image.event.ImagesDeleted
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+
+@Service
+class ImageServiceImpl(
+    private val imageRepository: ImageRepository,
+    private val client: AmazonS3Client,
+) : ImageService {
+    companion object {
+        private const val BUCKET_NAME = "guam"
+        private val imageLocation = Paths.get("image")
+    }
+
+    override fun createImages(command: CreateImages): ImagesCreated = command.run {
+        val images = imageRepository.saveAll(files.indices.map { ImageEntity(type = type.name, parentId = parentId) })
+
+        images.forEachIndexed { i, image ->
+            client.putObject(PutObjectRequest(BUCKET_NAME, image.getPath(), files[i].getFile()))
+        }
+
+        ImagesCreated(
+            imageIds = images.map { it.id },
+            imageType = type,
+            parentId = parentId
+        )
+    }
+
+    override fun deleteImages(command: DeleteImages): ImagesDeleted =
+        when (command) {
+            is DeleteImages.ById -> {
+                imageRepository.findAllById(command.imageIds)
+            }
+            is DeleteImages.ByParentId -> {
+                imageRepository.findByParentIdAndType(command.parentId, command.imageType.name)
+            }
+        }.let {
+            imageRepository.deleteAll(it)
+            ImagesDeleted(imageIds = it.map { it.id })
+        }
+
+    private fun ImageEntity.getPath() = "$type/$id"
+
+    private fun MultipartFile.getFile(): File = inputStream.use { inputStream ->
+        imageLocation.resolve(originalFilename).let {
+            Files.copy(inputStream, it, StandardCopyOption.REPLACE_EXISTING)
+            it.toFile()
+        }
+    }
+}
