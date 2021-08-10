826aa3ee5f1f92ea9a64cb0fd49f211166258271
jon <lgock2023@gmail.com>
2021-08-10 13:13:52 +0900

    [NT] feat: db module setting

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":test"))
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     runtimeOnly("com.h2database:h2")
