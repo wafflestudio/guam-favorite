057c2ffa54824a157ac76bb541ad36ca197fee45
jon <lgock2023@gmail.com>
2021-08-10 23:32:17 +0900

    [GUAM-118] feat: setup test

--- service/src/main/kotlin/EventPublisher.kt
+++ service/src/main/kotlin/EventPublisher.kt
@@ -5,8 +5,10 @@ import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Pointcut
 import org.springframework.context.ApplicationEventPublisher
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
+@Profile("!test")
 @Aspect
 @Component
 class EventPublisher(
