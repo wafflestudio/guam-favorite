aafd5a09ff458ba173d34c4b84d60f12f27fe37b
jon <lgock2023@gmail.com>
2021-08-10 18:20:27 +0900

    [GUAM-117] feat: update rows at once

--- db/src/main/kotlin/task/TaskRepository.kt
+++ db/src/main/kotlin/task/TaskRepository.kt
@@ -2,7 +2,13 @@ package waffle.guam.task
 
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
 
 interface TaskRepository : JpaRepository<TaskEntity, Long> {
     fun findAll(spec: Specification<TaskEntity>): List<TaskEntity>
+
+    @Modifying
+    @Query("UPDATE TaskEntity t SET t.userState = :userState WHERE t.id in :ids")
+    fun updateStates(ids: List<Long>, userState: String): Int
 }
