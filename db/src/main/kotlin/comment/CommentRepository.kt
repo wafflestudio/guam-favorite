e4dd29f730ff5abcae9acbb9f3ea64140a7bf537
Jeong Satchel <bugod96@gmail.com>
2021-08-11 19:03:28 +0900

    [GUAM-109] join fetch for findByThreadId

--- db/src/main/kotlin/comment/CommentRepository.kt
+++ db/src/main/kotlin/comment/CommentRepository.kt
@@ -1,8 +1,16 @@
 package waffle.guam.comment
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface CommentRepository : JpaRepository<CommentEntity, Long> {
+
+    @Query(
+        "SELECT c " +
+            "FROM threads t JOIN FETCH comments c " +
+            "ON c.thread_id = :threadId"
+    )
     fun findByThreadId(threadId: Long): List<CommentEntity>
+
     fun countByThreadId(threadId: Long): Long
 }
