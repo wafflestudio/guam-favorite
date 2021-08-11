6f60c950cdf273e20b50c756b4b5378fd5121e94
Jeong Satchel <bugod96@gmail.com>
2021-08-12 03:03:54 +0900

    [GUAM-109] back to one comment service impl, fetch join query update

--- db/src/main/kotlin/comment/CommentRepository.kt
+++ db/src/main/kotlin/comment/CommentRepository.kt
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query
 interface CommentRepository : JpaRepository<CommentEntity, Long> {
 
     @Query(
-        "SELECT c " +
-            "FROM threads t JOIN FETCH comments c " +
-            "ON c.thread_id = :threadId"
+        "SELECT c FROM CommentView c " +
+            "JOIN FETCH threads t " +
+            "JOIN FETCH c.user " +
+            "LEFT JOIN FETCH c.images " +
+            "WHERE c.thread_id = :threadId"
     )
     fun findByThreadId(threadId: Long): List<CommentEntity>
 
