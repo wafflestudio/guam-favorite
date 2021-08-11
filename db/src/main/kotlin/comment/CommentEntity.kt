ba8e2d9d12a5b6af2c48a7cbed26071cef3c1d1a
Jeong Satchel <bugod96@gmail.com>
2021-08-11 11:50:18 +0900

    [GUAM-109] db - thread, comment entity & repository

---
+++ db/src/main/kotlin/comment/CommentEntity.kt
@@ -0,0 +1,57 @@
+package waffle.guam.comment
+
+import waffle.guam.image.ImageEntity
+import waffle.guam.user.UserEntity
+import java.time.Instant
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
+@Table(name = "comments")
+@Entity
+data class CommentEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    val threadId: Long,
+
+    @Column(name = "user_id")
+    val userId: Long,
+
+    val content: String?,
+
+    val createdAt: Instant = Instant.now(),
+
+    val modifiedAt: Instant = createdAt,
+)
+
+@Table(name = "comments")
+@Entity
+data class CommentView(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    val threadId: Long,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    val user: UserEntity,
+
+    @OneToMany(mappedBy = "parentId", fetch = FetchType.LAZY)
+    val images: List<ImageEntity>,
+
+    val content: String?,
+
+    val createdAt: Instant = Instant.now(),
+
+    val modifiedAt: Instant = createdAt,
+)
---
+++ db/src/main/kotlin/thread/ThreadRepository.kt
@@ -0,0 +1,17 @@
+package waffle.guam.thread
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+interface ThreadRepository : JpaRepository<ThreadEntity, Long> {
+
+    fun findByUserIdAndProjectId(userId: Long, projectId: Long): Optional<ThreadEntity>
+
+    fun countByUserIdAndProjectId(userId: Long, projectId: Long): Int
+}
+
+interface ThreadViewRepository : JpaRepository<ThreadView, Long> {
+    fun findByProjectId(projectId: Long, pageable: Pageable): Page<ThreadView>
+}
