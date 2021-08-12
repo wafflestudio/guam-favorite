9f4bb90b2e333b9cf7542b487a629b59e17211be
Jhvictor4 <jhvictor4@wafflestudio.com>
2021-08-12 13:07:49 +0900

    ** task 쪽 UserState 변경
    project 삭제 / 성공적으로 종료 두 가지 경우가 나누어지지 않아 있었던 것을
    리팩토링 겸사겸사 쪼개두었습니다.
    
    아직 컨트롤러에도 붙이지 않았긴 하지만,
    
    이에 따라 State의 분리가 필요해졌습니다.
    task도 contributed 필드를 하나 더 정의해서, 추후 아카이브 등으로 쓰는게 어떨지 싶어 따로 넣어봤습니다.
    
    제 도메인이 아니긴 하니까 피드백 주세용

--- service/src/main/kotlin/task/model/UserState.kt
+++ service/src/main/kotlin/task/model/UserState.kt
@@ -1,7 +1,7 @@
 package waffle.guam.task.model
 
 enum class UserState {
-    GUEST, MEMBER, LEADER, QUIT, DECLINED;
+    GUEST, MEMBER, LEADER, QUIT, DECLINED, CONTRIBUTED;
 
     fun isValidMember(): Boolean = this == GUEST || this == MEMBER || this == LEADER
 }
