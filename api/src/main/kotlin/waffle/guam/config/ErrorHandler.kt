ad0327b07de8fdf87c1d0bf98b242cde6443ce23
Jhvictor4 <jhvictor4@wafflestudio.com>
2021-08-12 12:56:10 +0900

    Exception Handling
    409 분리 및 joinException code 세분화

--- api/src/main/kotlin/waffle/guam/config/ErrorHandler.kt
+++ api/src/main/kotlin/waffle/guam/config/ErrorHandler.kt
@@ -4,25 +4,30 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import waffle.guam.DataNotFoundException
-import waffle.guam.InvalidRequestException
-import waffle.guam.JoinException
-import waffle.guam.NotAllowedException
+import waffle.guam.*
 import waffle.guam.common.InvalidFirebaseTokenException
 
 @ControllerAdvice
 class ErrorHandler {
     @ExceptionHandler(value = [DataNotFoundException::class])
     fun notfound(e: RuntimeException) =
-        ResponseEntity(ErrorResponse(e.message ?: ""), HttpStatus.NOT_FOUND)
+        ResponseEntity(ErrorResponse(e.message ?: "404 not found"), HttpStatus.NOT_FOUND)
 
     @ExceptionHandler(value = [InvalidFirebaseTokenException::class, InvalidRequestException::class])
-    fun baeRequest(e: RuntimeException) =
-        ResponseEntity(ErrorResponse(e.message ?: ""), HttpStatus.BAD_REQUEST)
+    fun badRequest(e: RuntimeException) =
+        ResponseEntity(ErrorResponse(e.message ?: "400 bad request"), HttpStatus.BAD_REQUEST)
 
-    @ExceptionHandler(value = [JoinException::class, NotAllowedException::class])
+    @ExceptionHandler(value = [NotAllowedException::class])
     fun notAllowed(e: RuntimeException) =
-        ResponseEntity(ErrorResponse(e.message ?: ""), HttpStatus.FORBIDDEN)
+        ResponseEntity(ErrorResponse(e.message ?: "403 forbidden"), HttpStatus.FORBIDDEN)
+
+    @ExceptionHandler(value = [ConflictException::class])
+    fun conflict(e: RuntimeException) =
+        ResponseEntity(ErrorResponse(e.message ?: "409 conflict"), HttpStatus.CONFLICT)
+
+    @ExceptionHandler(value = [JoinException::class])
+    fun joinExceptionOccurred(e: JoinException) =
+        ResponseEntity(ErrorResponse(e.message ?: "join exception occurred"), e.code)
 }
 
 data class ErrorResponse(
