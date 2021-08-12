d864e0dbf3f5022b5f562fc6db9e3c389f44e5fe
wlgur <jhvictor4@wafflestudio.com>
2021-08-12 20:04:24 +0900

    stack thumbnail fetch
    누락된 부분 N+1 없도록.

--- db/src/main/kotlin/stack/StackRepository.kt
+++ db/src/main/kotlin/stack/StackRepository.kt
@@ -1,5 +1,14 @@
 package waffle.guam.stack
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.Optional
 
-interface StackRepository : JpaRepository<StackEntity, Long>
+interface StackRepository : JpaRepository<StackEntity, Long> {
+
+    @Query("select distinct s from StackEntity s left join fetch s.thumbnail t where s.id = :stackId")
+    override fun findById(stackId: Long) : Optional<StackEntity>
+
+    @Query("select distinct s from StackEntity s left join fetch s.thumbnail t where s.id in :ids")
+    fun findAllByIds(ids: List<Long>): List<StackEntity>
+}
