58de3cb96f5aa04db0af813a20cfd1792706c585
Jeong Satchel <bugod96@gmail.com>
2021-08-13 10:15:32 +0900

    [GUAM-109] comment controller setup for delete comment image

---
+++ api/src/main/kotlin/waffle/guam/controller/request/Request.kt
@@ -0,0 +1,23 @@
+package waffle.guam.controller.request
+
+import org.springframework.web.multipart.MultipartFile
+import waffle.guam.taskmessage.model.TaskStatus
+
+data class ContentInput(
+    val content: String
+)
+
+data class CreateFullInfoInput(
+    val content: String?,
+    val imageFiles: List<MultipartFile>?
+)
+
+data class CreateTaskMsgInput(
+    val msg: String,
+    val status: TaskStatus
+)
+
+data class UpdateTaskMsgInput(
+    val msg: String?,
+    val status: TaskStatus?
+)
