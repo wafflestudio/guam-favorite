ded70461ddcedb30bcd9e9707e4cd57f346a5525
Jeong Satchel <bugod96@gmail.com>
2021-08-13 12:13:34 +0900

    [GUAM-109] remove useless findByThreadId - should use countByThreadId

--- db/src/main/kotlin/comment/CommentRepository.kt
+++ db/src/main/kotlin/comment/CommentRepository.kt
@@ -1,17 +1,8 @@
 package waffle.guam.comment
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
 interface CommentRepository : JpaRepository<CommentEntity, Long> {
-
-    @Query(
-        "SELECT c FROM CommentView c " +
-            "JOIN FETCH c.user " +
-            "LEFT JOIN FETCH c.images " +
-            "WHERE c.thread_id = :threadId"
-    )
-    fun findByThreadId(threadId: Long): List<CommentEntity>
 
     fun countByThreadId(threadId: Long): Long
 }
