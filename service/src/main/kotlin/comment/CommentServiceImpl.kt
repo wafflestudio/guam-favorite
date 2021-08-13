7a17ff8d0b7eb9ad92add4a31f47ab25ef9b43f4
Jeong Satchel <bugod96@gmail.com>
2021-08-13 11:11:12 +0900

    [GUAM-109] refactor createComment - validCommentCreator, threadRepository

--- service/src/main/kotlin/comment/CommentServiceImpl.kt
+++ service/src/main/kotlin/comment/CommentServiceImpl.kt
@@ -17,31 +17,22 @@ import waffle.guam.image.command.DeleteImages
 import waffle.guam.task.TaskService
 import waffle.guam.task.command.SearchTask
 import waffle.guam.task.model.UserState
-import waffle.guam.thread.ThreadService
-import waffle.guam.thread.command.GetThreadInfo
+import waffle.guam.thread.ThreadRepository
 import java.time.Instant
 
 @Service
 class CommentServiceImpl(
     private val commentRepository: CommentRepository,
-    private val threadService: ThreadService,
+    private val threadRepository: ThreadRepository,
     private val taskService: TaskService,
     private val imageService: ImageService
 ) : CommentService {
+
     override fun createComment(command: CreateComment): CommentCreated {
-        val parentThread = threadService.getThreadInfo(GetThreadInfo.ByIds(listOf(command.threadId)))
-            .firstOrNull() ?: throw DataNotFoundException()
-        val task = taskService.getTasks(SearchTask(listOf(command.userId), listOf(parentThread.projectId)))
-            .firstOrNull() ?: throw DataNotFoundException()
-        if (task.userState == UserState.GUEST) {
-            val joinRequestThread = threadService.getThreadInfo(
-                GetThreadInfo.ByUserIdAndProjectId(command.userId, parentThread.projectId)
-            ).firstOrNull() ?: throw DataNotFoundException()
-            if (joinRequestThread.id != command.threadId) throw NotAllowedException("아직 다른 쓰레드에 댓글을 생성할 권한이 없습니다.")
+        validateCommentCreator(command)
+        commentRepository.save(command.copy(content = command.content?.trim()).toEntity()).let {
+            return CommentCreated(it.id, command.imageFiles)
         }
-        if (task.userState in listOf(UserState.QUIT, UserState.DECLINED)) throw NotAllowedException("해당 프로젝트에 댓글을 생성할 권한이 없습니다.")
-        val createdComment = commentRepository.save(command.copy(content = command.content?.trim()).toEntity())
-        return CommentCreated(createdComment.id, command.imageFiles)
     }
 
     override fun editCommentContent(command: EditCommentContent): CommentContentEdited {
@@ -64,5 +55,23 @@ class CommentServiceImpl(
         if (targetComment.userId != command.userId) throw NotAllowedException("타인이 작성한 댓글을 삭제할 수는 없습니다.")
         commentRepository.deleteById(command.commentId)
         return CommentDeleted(command.commentId, targetComment.threadId)
+    }
+
+    protected fun validateCommentCreator(command: CreateComment) {
+        val parentThread = threadRepository.findById(command.threadId).orElseThrow(::DataNotFoundException)
+
+        val task = taskService.getTasks(SearchTask(listOf(command.userId), listOf(parentThread.projectId)))
+            .firstOrNull() ?: throw DataNotFoundException() // TODO(fix to getTask after merge)
+
+        if (task.userState == UserState.GUEST) {
+            val joinRequestThread = threadRepository.findByUserIdAndProjectId(command.userId, parentThread.projectId)
+                .orElseThrow(::DataNotFoundException)
+            if (joinRequestThread.id != command.threadId) {
+                throw NotAllowedException("아직 다른 쓰레드에 댓글을 생성할 권한이 없습니다.")
+            }
+        }
+        if (task.userState in listOf(UserState.QUIT, UserState.DECLINED)) {
+            throw NotAllowedException("해당 프로젝트에 댓글을 생성할 권한이 없습니다.")
+        }
     }
 }
