0b26a6cd377145b62b4f0268ffe862342ea0e4f5
Jeong Satchel <bugod96@gmail.com>
2021-08-14 14:43:30 +0900

    [GUAM-129] refactor: taskMessageServiceImpl, commands setup to match master

--- service/src/main/kotlin/taskmessage/command/CreateTaskMessage.kt
+++ service/src/main/kotlin/taskmessage/command/CreateTaskMessage.kt
@@ -1,16 +1,18 @@
 package waffle.guam.taskmessage.command
 
+import waffle.guam.taskmessage.TaskMessageEntity
 import waffle.guam.taskmessage.model.TaskStatus
 
 data class CreateTaskMessage(
+    val userId: Long,
     val taskId: Long,
     val msg: String,
     val status: TaskStatus
 ) : TaskMessageCommand {
-//    fun toEntity(): TaskMessage =
-//        TaskMessage(
-//            msg = msg,
-//            status = status,
-//            taskId = taskId
-//        )
+    fun toEntity(): TaskMessageEntity =
+        TaskMessageEntity(
+            msg = msg,
+            status = status.name,
+            taskId = taskId
+        )
 }
