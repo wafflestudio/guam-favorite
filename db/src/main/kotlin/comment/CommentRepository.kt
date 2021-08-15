482ca143cbb0ad766885d810ef16ce33649a99d7
Jeong Satchel <bugod96@gmail.com>
2021-08-15 15:07:17 +0900

    [GUAM-130] if-else to when syntax, count to exists

--- db/src/main/kotlin/comment/CommentRepository.kt
+++ db/src/main/kotlin/comment/CommentRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface CommentRepository : JpaRepository<CommentEntity, Long> {
 
     fun countByThreadId(threadId: Long): Long
+
+    fun existsByThreadId(threadId: Long): Boolean
 }
