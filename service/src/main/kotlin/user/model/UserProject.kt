c78110c0efbc55b714c1be9ddf26949fa8f6af5e
jon <lgock2023@gmail.com>
2021-08-15 16:12:43 +0900

    [GUAM-119] feat: UserServiceImpl

--- service/src/main/kotlin/user/model/UserProject.kt
+++ service/src/main/kotlin/user/model/UserProject.kt
@@ -1,5 +1,7 @@
 package waffle.guam.user.model
 
+import waffle.guam.image.model.Image.Companion.toDomain
+import waffle.guam.task.TaskEntity
 import waffle.guam.task.model.Position
 import waffle.guam.task.model.UserState
 
@@ -8,5 +10,15 @@ data class UserProject(
     val projectTitle: String,
     val projectThumbnail: String?,
     val position: Position,
-    val userState: UserState
-)
+    val userState: UserState,
+) {
+    companion object {
+        fun of(e: TaskEntity) = UserProject(
+            projectId = e.project.id,
+            projectTitle = e.project.title,
+            projectThumbnail = e.project.thumbnail?.toDomain()?.path,
+            position = Position.valueOf(e.position),
+            userState = UserState.valueOf(e.userState)
+        )
+    }
+}
