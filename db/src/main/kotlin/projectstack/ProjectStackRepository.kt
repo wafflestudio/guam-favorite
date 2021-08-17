dc9f5a6f81a6fc975dc7d111fab53a43cbd23234
jon <lgock2023@gmail.com>
2021-08-18 01:42:31 +0900

    [GUAM-141] refactor: PrjStackServiceImpl

--- db/src/main/kotlin/projectstack/ProjectStackRepository.kt
+++ db/src/main/kotlin/projectstack/ProjectStackRepository.kt
@@ -1,8 +1,8 @@
 package waffle.guam.projectstack
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
-import java.util.Optional
 
 interface ProjectStackRepository : JpaRepository<ProjectStackEntity, Long> {
 
@@ -12,5 +12,7 @@ interface ProjectStackRepository : JpaRepository<ProjectStackEntity, Long> {
     @Query("select distinct s from ProjectStackEntity s inner join fetch s.techStack t where s.projectId in :projectIds")
     fun findAllByProjectIds(projectIds: List<Long>): List<ProjectStackEntity>
 
-    fun findByProjectIdAndPosition(projectId: Long, position: String): Optional<ProjectStackEntity>
+    @Query("delete from ProjectStackEntity s where s.projectId = :projectId and s.position in :positions")
+    @Modifying
+    fun deleteAllByProjectIdAndPositionIn(projectId: Long, positions: List<String>): Int
 }
--- service/src/main/kotlin/projectstack/PrjStackServiceImpl.kt
+++ service/src/main/kotlin/projectstack/PrjStackServiceImpl.kt
@@ -16,34 +16,33 @@ class PrjStackServiceImpl(
 ) : ProjectStackService {
 
     override fun getProjectStacks(projectId: Long): List<ProjectStack> =
-
         projectStackRepository.findByProjectId(projectId).map { ProjectStack.of(it) }
 
     override fun getAllProjectStacks(projectIds: List<Long>): List<ProjectStack> =
-
         projectStackRepository.findAllByProjectIds(projectIds).map { ProjectStack.of(it) }
 
     @Transactional
-    override fun createProjectStacks(projectId: Long, command: StackIdList): ProjectStacksCreated {
-        val list = projectStackRepository.saveAll(buildPrjStackList(command, projectId))
-
-        return ProjectStacksCreated(projectId, list.map { it.id })
-    }
+    override fun createProjectStacks(projectId: Long, command: StackIdList): ProjectStacksCreated =
+        buildPrjStackList(command, projectId)
+            .let {
+                projectStackRepository.saveAll(it)
+            }.let {
+                ProjectStacksCreated(projectId, it.map { it.id })
+            }
 
     @Transactional
-    override fun updateProjectStacks(projectId: Long, command: StackIdList): ProjectStacksUpdated {
-
-        // list 안에 존재하는 포지션의 스택만 덮어씀.
-        val list = buildPrjStackList(command, projectId)
-        val entities = list.map {
-            projectStackRepository
-                .findByProjectIdAndPosition(projectId, it.position).orElseThrow(::DataNotFoundException)
-        }
-        projectStackRepository.deleteAllInBatch(entities)
-
-        projectStackRepository.saveAll(list)
-        return ProjectStacksUpdated(projectId, list.map { it.id })
-    }
+    override fun updateProjectStacks(projectId: Long, command: StackIdList): ProjectStacksUpdated =
+        buildPrjStackList(command, projectId)
+            .apply {
+                projectStackRepository.deleteAllByProjectIdAndPositionIn(
+                    projectId = projectId,
+                    positions = map { it.position }
+                )
+            }.let {
+                projectStackRepository.saveAll(it)
+            }.let {
+                ProjectStacksUpdated(projectId, it.map { it.id })
+            }
 
     private fun buildPrjStackList(stackIds: StackIdList, projectId: Long): List<ProjectStackEntity> =
         stackRepository.findAllById(stackIds.validList)
