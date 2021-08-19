7e6dd7320a6d78c104097c326a0dbc3174383d82
Jeong Satchel <bugod96@gmail.com>
2021-08-19 21:52:20 +0900

    [GUAM-131] comment service - FeatureSpec to @Transactional, undo fetch join

--- db/src/main/kotlin/comment/CommentRepository.kt
+++ db/src/main/kotlin/comment/CommentRepository.kt
@@ -1,17 +1,8 @@
 package waffle.guam.comment
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
-import java.util.Optional
 
 interface CommentRepository : JpaRepository<CommentEntity, Long> {
-
-    @Query(
-        "select c " +
-            "from CommentEntity c left join fetch c.images i " +
-            "where c.id = :commentId"
-    )
-    override fun findById(commentId: Long): Optional<CommentEntity>
 
     fun countByThreadId(threadId: Long): Long
 
