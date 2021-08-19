562925dbb5bf97204589e103f19b0bd7f51cd9a3
Jeong Satchel <bugod96@gmail.com>
2021-08-20 01:56:17 +0900

    [GUAM-131] refactor: of create commands

--- service/src/main/kotlin/comment/event/CommentCreated.kt
+++ service/src/main/kotlin/comment/event/CommentCreated.kt
@@ -1,6 +1,9 @@
 package waffle.guam.comment.event
 
 import org.springframework.web.multipart.MultipartFile
+import waffle.guam.comment.command.CreateComment
+import waffle.guam.thread.ThreadEntity
+import waffle.guam.user.UserEntity
 import java.time.Instant
 
 data class CommentCreated(
@@ -12,4 +15,17 @@ data class CommentCreated(
     val content: String,
     val imageFiles: List<MultipartFile>?,
     override val timestamp: Instant = Instant.now()
-) : CommentEvent(timestamp)
+) : CommentEvent(timestamp) {
+    companion object {
+        fun of(thread: ThreadEntity, commentId: Long, command: CreateComment, creator: UserEntity): CommentCreated =
+            CommentCreated(
+                projectId = thread.projectId,
+                commentId = commentId,
+                threadCreatorId = thread.userId,
+                commentCreatorId = command.userId,
+                commentCreatorName = creator.nickname,
+                content = command.content ?: "이미지 등록",
+                imageFiles = command.imageFiles
+            )
+    }
+}
--- service/src/main/kotlin/thread/event/ThreadCreated.kt
+++ service/src/main/kotlin/thread/event/ThreadCreated.kt
@@ -1,6 +1,9 @@
 package waffle.guam.thread.event
 
 import org.springframework.web.multipart.MultipartFile
+import waffle.guam.project.ProjectEntity
+import waffle.guam.task.model.Task
+import waffle.guam.thread.command.CreateThread
 import java.time.Instant
 
 data class ThreadCreated(
@@ -11,4 +14,16 @@ data class ThreadCreated(
     val content: String,
     val imageFiles: List<MultipartFile>?,
     override val timestamp: Instant = Instant.now(),
-) : ThreadEvent(timestamp)
+) : ThreadEvent(timestamp) {
+    companion object {
+        fun of(project: ProjectEntity, threadId: Long, command: CreateThread, task: Task): ThreadCreated =
+            ThreadCreated(
+                projectId = project.id,
+                threadId = threadId,
+                creatorId = command.userId,
+                creatorName = task.user.nickname,
+                content = command.content ?: "이미지 등록",
+                imageFiles = command.imageFiles,
+            )
+    }
+}
