1dd3ac26c20f09c9fff404e38534229c71b41793
Jhvictor4 <wlgur7238@snu.ac.kr>
2021-08-23 11:44:26 +0900

    prj api draft
    ㅜ 일처리 느려서 죄송..
    task 쪽 서비스 피처 + 테스트코드
    넣어서 곧 마무리 할게요

---
+++ api/src/main/kotlin/api/request/UpdateProjectRequest.kt
@@ -0,0 +1,33 @@
+package waffle.guam.api.request
+
+import org.springframework.web.multipart.MultipartFile
+import waffle.guam.project.command.UpdateProject
+import waffle.guam.project.model.Due
+
+data class UpdateProjectRequest(
+    val title: String?,
+    val description: String?,
+    var imageFiles: MultipartFile?,
+    val frontHeadCnt: Int,
+    val backHeadCnt: Int,
+    val designHeadCnt: Int,
+    val frontStackId: Long?,
+    val backStackId: Long?,
+    val designStackId: Long?,
+    val due: Due?
+) {
+    fun toCommand() : UpdateProject =
+        UpdateProject(
+            title = title,
+            description = description,
+            imageFiles = imageFiles,
+            frontHeadCnt = frontHeadCnt,
+            backHeadCnt = backHeadCnt,
+            designHeadCnt = designHeadCnt,
+            frontStackId = frontStackId,
+            backStackId = backStackId,
+            designStackId = designStackId,
+            due = due
+        )
+
+}
