3ef96eb178770dc45317eb1b9eb1c83c4497d682
jon <lgock2023@gmail.com>
2021-08-27 00:18:00 +0900

    feat: fix image

--- service/src/main/kotlin/user/event/UserEventHandler.kt
+++ service/src/main/kotlin/user/event/UserEventHandler.kt
@@ -3,14 +3,18 @@ package waffle.guam.user.event
 import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import waffle.guam.image.ImageRepository
 import waffle.guam.image.ImageService
 import waffle.guam.image.command.CreateImages
 import waffle.guam.image.command.DeleteImages
 import waffle.guam.image.model.ImageType
+import waffle.guam.user.UserRepository
 
 @Component
 class UserEventHandler(
     private val imageService: ImageService,
+    private val userRepository: UserRepository,
+    private val imageRepository: ImageRepository
 ) {
     private val logger = LoggerFactory.getLogger(this::javaClass.name)
 
@@ -27,14 +31,17 @@ class UserEventHandler(
 
         when (e.image == null) {
             true -> {
+                userRepository.findById(e.userId).get().image = null
                 imageService.deleteImages(
                     command = DeleteImages.ByParentId(parentId = e.userId, imageType = ImageType.PROFILE)
                 )
             }
             false -> {
-                imageService.createImages(
+                val event = imageService.createImages(
                     command = CreateImages(files = listOf(e.image), type = ImageType.PROFILE, parentId = e.userId)
                 )
+                userRepository.findById(e.userId).get().image =
+                    imageRepository.findById(event.imageIds.first()).get()
             }
         }
     }
