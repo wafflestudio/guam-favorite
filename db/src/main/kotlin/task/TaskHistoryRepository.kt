b77cd8a57a09c77447a46d99398756d8cb0ffcfe
jon <lgock2023@gmail.com>
2021-08-26 23:15:11 +0900

    [NT] feat: user projectIds contians task history

--- db/src/main/kotlin/task/TaskHistoryRepository.kt
+++ db/src/main/kotlin/task/TaskHistoryRepository.kt
@@ -2,4 +2,6 @@ package waffle.guam.task
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface TaskHistoryRepository : JpaRepository<TaskHistoryEntity, Long>
+interface TaskHistoryRepository : JpaRepository<TaskHistoryEntity, Long> {
+    fun findAllByUserId(userId: Long): List<TaskHistoryEntity>
+}
