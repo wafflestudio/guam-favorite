28f6d490028db1398c5907c51756d642e5b77274
Jeong Satchel <bugod96@gmail.com>
2021-09-01 20:48:58 +0900

    [GUAM-149] join thread can not be empty

--- service/src/main/kotlin/thread/command/CreateJoinThread.kt
+++ service/src/main/kotlin/thread/command/CreateJoinThread.kt
@@ -1,5 +1,6 @@
 package waffle.guam.thread.command
 
+import waffle.guam.InvalidRequestException
 import waffle.guam.thread.ThreadEntity
 import waffle.guam.thread.model.ThreadType
 
@@ -8,5 +9,10 @@ data class CreateJoinThread(
     val userId: Long,
     val content: String
 ) : ThreadCommand {
+    init {
+        if (content.isBlank()) {
+            throw InvalidRequestException("입력된 내용이 없습니다.")
+        }
+    }
     fun toEntity() = ThreadEntity(projectId = projectId, userId = userId, content = content, type = ThreadType.JOIN.name)
 }
