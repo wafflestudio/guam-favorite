3a2cadfcf1bf1a7aae8fcb30d676ccd1f7d2b379
jon <lgock2023@gmail.com>
2021-09-05 22:51:50 +0900

    [NT] fix: complete project

--- api/src/main/kotlin/api/KakaoController.kt
+++ api/src/main/kotlin/api/KakaoController.kt
@@ -2,6 +2,7 @@ package waffle.guam.api
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserRecord
+import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,10 +15,11 @@ import reactor.core.publisher.Mono
 @RequestMapping("/kakao")
 class KakaoController {
     private val webClient: WebClient = WebClient.create("https://kapi.kakao.com/v2/user/me")
+    private val logger = LoggerFactory.getLogger(this::class.java.name)
 
     @GetMapping("")
     fun getFirebaseToken(
-        @RequestParam token: String
+        @RequestParam token: String,
     ): Mono<Response> =
         webClient.get()
             .headers { it.set("Authorization", "Bearer $token") }
@@ -27,9 +29,7 @@ class KakaoController {
             .map { res ->
                 val firebaseUid =
                     getUserWithUid(res.id) ?: FirebaseAuth.getInstance().createUser(
-                        UserRecord.CreateRequest().also {
-                            it.setUid("refactor:${res.id}")
-                        }
+                        UserRecord.CreateRequest().also { it.setUid("refactor:${res.id}") }
                     ).uid
 
                 Response(
@@ -38,10 +38,17 @@ class KakaoController {
             }
 
     fun getUserWithUid(uid: Int): String? =
-        kotlin.runCatching { FirebaseAuth.getInstance().getUser("refactor:$uid").uid }.getOrNull()
+        kotlin.runCatching { FirebaseAuth.getInstance().getUser("refactor:$uid").uid }.let {
+            if (it.isFailure) {
+                logger.error("Get firebaseUser failed with uid [refactor:$uid]", it.exceptionOrNull())
+                null
+            } else {
+                it.getOrNull()!!
+            }
+        }
 
     data class Response(
-        val customToken: String
+        val customToken: String,
     )
 }
 
@@ -49,7 +56,7 @@ private data class RequestMeResponse(
     val connected_at: String?,
     val id: Int,
     val kakao_account: KakaoAccount,
-    val properties: Properties?
+    val properties: Properties?,
 )
 
 private data class KakaoAccount(
@@ -68,18 +75,18 @@ private data class KakaoAccount(
     val is_email_valid: Boolean,
     val is_email_verified: Boolean,
     val profile: Profile?,
-    val profile_needs_agreement: Boolean?
+    val profile_needs_agreement: Boolean?,
 )
 
 private data class Profile(
     val is_default_image: Boolean,
     val nickname: String,
     val profile_image_url: String,
-    val thumbnail_image_url: String
+    val thumbnail_image_url: String,
 )
 
 private data class Properties(
     val nickname: String,
     val profile_image: String,
-    val thumbnail_image: String
+    val thumbnail_image: String,
 )
