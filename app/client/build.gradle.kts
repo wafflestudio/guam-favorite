58f2bca6e09270c3d9b5e1ddd508a64afcdc16d7
jon <lgock2023@gmail.com>
2022-09-23 19:42:01 +0900

    fix dependencies

--- app/client/build.gradle.kts
+++ app/client/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
-    implementation(project(":model"))
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    api(project(":model"))
+    compileOnly("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 publishing {
